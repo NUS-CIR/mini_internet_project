61094103248a2c40a173b6dd0e214803175b764b
Xin Zhe Khooi <khooixz@comp.nus.edu.sg>
2022-08-01 10:49:40 +0800

    updated save_configs

--- platform/setup/save_configs.sh
+++ platform/setup/save_configs.sh
@@ -105,7 +105,7 @@ for ((k=0;k<n_groups;k++)); do
 
     echo "zip \${dirname}.zip \${dirname}/*" >> "${file_loc}"
     echo "echo \"Download zip file:\"" >> "${file_loc}"
-    echo "echo \"    scp -P $((2000 + ${group_number})) root@duvel.ethz.ch:\${dirname}.zip .\"" >> "${file_loc}"
+    echo "echo \"    scp -i g${group_number} -J cs3103@ocna0.d2.comp.nus.edu.sg -P $((2000 + ${group_number})) root@mini-internet:\${dirname}.zip .\"" >> "${file_loc}"
     echo "echo \"Overwrite the config folder in the current directory:\"" >> "${file_loc}"
-    echo "echo \"    scp -r -P $((2000 + ${group_number})) root@duvel.ethz.ch:\${dirname} config\"" >> "${file_loc}"
+    echo "echo \"    scp -i g${group_number} -J cs3103@ocna0.d2.comp.nus.edu.sg -r -P $((2000 + ${group_number})) root@mini-internet:\${dirname} config\"" >> "${file_loc}"
 done
