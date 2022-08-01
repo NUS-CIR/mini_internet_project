77cf7f9892171efd1a380ae46dd85d913e59ca49
Xin Zhe Khooi <khooixz@comp.nus.edu.sg>
2022-08-01 10:41:58 +0800

    old to new mappings

---
+++ platform/config/change_name.sh
@@ -0,0 +1,12 @@
+#! /bin/bash 
+
+sed -i s/FIFA/MOE/g *
+sed -i s/UEFA/MOH/g *
+sed -i s/BASE/BTIMAH/g *
+sed -i s/GENE/KRIDGE/g *
+sed -i s/LUGA/NOVENA/g *
+sed -i s/ZURI/BISHAN/g *
+sed -i s/MUNI/KRANJI/g *
+sed -i s/LYON/JURONG/g *
+sed -i s/MILA/MARINA/g *
+sed -i s/VIEN/CHANGI/g *
